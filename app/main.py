66e18b6b8f71969b225633c3cb219e36f0894212
Artem <artem@MacBook-Pro-Artem.local>
2024-02-07 16:30:59 +0530

    added ssl

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ from fastapi.templating import Jinja2Templates
 import starlette.status as status
 from .library.helpers import *
 
-app = FastAPI(debug=True, ssl_keyfile="app/adamanto.pro.key", ssl_certfile="app/certificate_ca.srt")
+app = FastAPI(debug=True, ssl_keyfile="app/adamanto.pro.key", ssl_certfile="app/certificate_ca.crt")
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
